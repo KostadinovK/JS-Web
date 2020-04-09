14eea41ed3594b37ada60413465ab01737155be5
KostadinovK <kostadinovk@gmail.com>
2020-04-09 10:15:56 +0300

    Added Footer and Aside components

--- 02-ReactJS/01-Components Workshop/React App/app/src/App/App.js
+++ 02-ReactJS/01-Components Workshop/React App/app/src/App/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Navigation from '../Navigation/Navigation';
+import Aside from '../Aside/Aside';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <div className="Container">
+        <Aside/>
       </div>
+      <Footer/>
     </div>
   );
 }
