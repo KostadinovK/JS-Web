90e297fa5ee1de066a136c2fd2ca37079d3f4e84
KostadinovK <kostadinovk@gmail.com>
2020-04-09 09:56:38 +0300

    Added navbar and link components

---
+++ 02-ReactJS/01-Components Workshop/React App/app/src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Navigation from '../Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <div className="Container">
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-ReactJS/01-Components Workshop/React App/app/src/shared/Link/Link.js
@@ -0,0 +1,12 @@
+import React, { Children } from 'react';
+import './Link.css';
+
+function Link( {children, url} ){
+    return (
+        <li className='listItem'>
+            <a href={url}>{children}</a>
+        </li>
+    );
+}
+
+export default Link;
