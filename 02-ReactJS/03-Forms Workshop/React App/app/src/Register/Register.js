d437841dce9196c1ae5dc5d6cf5a4f015722d4e2
KostadinovK <kostadinovk@gmail.com>
2020-04-11 10:00:14 +0300

    Added user service and register functionality without validations for now

--- 02-ReactJS/03-Forms Workshop/React App/app/src/Register/Register.js
+++ 02-ReactJS/03-Forms Workshop/React App/app/src/Register/Register.js
@@ -2,29 +2,63 @@ import React from 'react';
 
 import '../shared/styles/LoginAndRegister.css';
 
-function Register() {
-    return (
-        <div className='Register'>
-            <h1>Register</h1>
-            <form>
-                <div className='form-control'>
-                    <label for='email'>Email</label>
-                    <input type='text' id='email'></input>
-                </div>
-                <div className='form-control'>
-                    <label for='pass'>Password</label>
-                    <input type='password' id='pass'></input>
-                </div>
-                <div className='form-control'>
-                    <label for='rePass'>Repeat Password</label>
-                    <input type='password' id='rePass'></input>
-                </div>
-                <div className='form-control'>
-                    <button type='submit'>Register</button>
-                </div>
-            </form>
-        </div>
-    );
+import service from '../services/userService';
+
+class Register extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            form: {},
+            errors: {}
+        };
+    };
+
+    onInputChange = (event) => {
+        const name = event.target.id;
+        const value = event.target.value;
+
+        this.setState(({ form }) => {
+            return { form: { ...form, [name]: value } };
+        });
+    };
+
+    onRegisterSubmit = async (event) => {
+        event.preventDefault();
+        
+        const {email, pass, rePass} = this.state.form;
+
+        await service.register(email, pass);
+        
+        this.props.history.push('/login');
+    };
+
+    render(){
+        return (
+            <div className='Register'>
+                <h1>Register</h1>
+                <form onSubmit={this.onRegisterSubmit}>
+                    <div className='form-control'>
+                        <label for='email'>Email</label>
+                        <input type='text' id='email' onChange={this.onInputChange}></input>
+                    </div>
+                    <div className='form-control'>
+                        <label for='pass'>Password</label>
+                        <input type='password' id='pass' onChange={this.onInputChange}></input>
+                    </div>
+                    <div className='form-control'>
+                        <label for='rePass'>Repeat Password</label>
+                        <input type='password' id='rePass' onChange={this.onInputChange}></input>
+                    </div>
+                    <div className='form-control'>
+                        <button type='submit'>Register</button>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+   
 }
 
 export default Register;
