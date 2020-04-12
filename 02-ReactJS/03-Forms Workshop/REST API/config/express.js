38ea4362fb04338cdb3b335fbd90adf7bbf63f78
KostadinovK <kostadinovk@gmail.com>
2020-04-12 09:18:46 +0300

    Added cors policy to api for workshop 03

--- 02-ReactJS/03-Forms Workshop/REST API/config/express.js
+++ 02-ReactJS/03-Forms Workshop/REST API/config/express.js
@@ -4,7 +4,20 @@ const cors = require('cors');
 const secret = 'secret';
 
 module.exports = (app) => {
-    app.use(cors());
+    let whitelist = ['http://localhost:3000'];
+
+    let corsOptions = {
+        credentials: true,
+        origin: function (origin, callback) {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        }
+    };
+
+    app.use(cors(corsOptions));
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
